24404caa81f13027d27dd1771c2a27c8dba233c6
Fr1nge-96 <effortfr1nge@gmail.com>
2024-02-01 21:13:16 +0300

    first commit

--- js.js
+++ js.js
@@ -3,14 +3,16 @@ document.getElementById('vector').addEventListener('click', toggleMenu)
 document.getElementById('blur').addEventListener('click', blurOff)
 document.getElementById('arrow__down').addEventListener('click', cursorClick)
 document.getElementById('arrow__down').addEventListener('click', cursorClickHidden)
-
+document.getElementById('pushForm').addEventListener('click', pushOrder)
+document.getElementById('orderCall').addEventListener('click', callOrder)
 
 function toggleMenu(){
-    document.getElementById('main__menu').classList.toggle('menu-visible')
+    document.getElementById('main__menu').classList.toggle('menu-visible');
+
 }
 
 function blurOff(){
-    document.getElementById('menu__blur').classList.add('menu__blur-remove')
+    document.getElementById('menu__blur').classList.add('menu__blur-remove');
 }
 
 function cursorClick() {
@@ -19,4 +21,11 @@ function cursorClick() {
 
 function cursorClickHidden() {
     document.getElementById('cursorUp').classList.toggle('arrow-down__top')
+}
+function pushOrder(){
+    document.getElementById('pushOrder').classList.toggle('form-order__container--visible');
+}
+
+function callOrder(){
+    document.getElementById('pushOrder').classList.toggle('form-order__container--visible');
 }
--- swiper.js
+++ swiper.js
@@ -1,6 +1,19 @@
 let swiper = new Swiper(".mySwiper", {
     spaceBetween: 20,
-    // slidesPerView: 2,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    // mousewheel: true,
+    keyboard: true,
+    loop: true,
+});
+
+let swiper2 = new Swiper(".swiper2", {
+    spaceBetween: 20,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
